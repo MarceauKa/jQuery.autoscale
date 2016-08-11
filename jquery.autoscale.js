eefd3b9fada328f9ebb4f56354f109cdf7864eae
MarceauKa <marceau@casals.fr>
2016-08-11 20:13:53 +0200

    typos

--- jquery.autoscale.js
+++ jquery.autoscale.js
@@ -2,65 +2,65 @@
  * Watch out! This is just a speed-coded functionnal experiment.
  *
  * @package jquery.autoscale
- * @author 	Marceau Casals <marceau@casals.fr>
- * @doc 	https://github.com/AkibaTech/jQuery.autoscale
+ * @author  Marceau Casals <marceau@casals.fr>
+ * @doc     https://github.com/AkibaTech/jQuery.autoscale
  */
 (function($) {
 
-	$.fn.autoscale = function(options) {
+    $.fn.autoscale = function(options) {
 
-		// Plugin apply on
-		var $el = $(this);
+        // Plugin apply on
+        var $el = $(this);
 
-		// Wrapper of the element
-		var $wrapper = $el.parent();
+        // Wrapper of the element
+        var $wrapper = $el.parent();
 
-		// Throttle implementation
-		function throttle(callback, ttr) {
-    		var throttling = false;
-    		return function () {
-        		if (!throttling) {
-            		callback.call();
-            		throttling = true;
-            		setTimeout(function () {
-                		throttling = false;
-            		}, ttr);
-        		}
-    		}
-    	};
+        // Throttle implementation
+        function throttle(callback, ttr) {
+            var throttling = false;
+            return function () {
+                if (!throttling) {
+                    callback.call();
+                    throttling = true;
+                    setTimeout(function () {
+                        throttling = false;
+                    }, ttr);
+                }
+            }
+        };
 
-		// Apply margins on element
-		var applyMargins = function() {
-			var marginLeft = $el.width() / 2,
-				marginTop = $el.height() / 2;
+        // Apply margins on element
+        var applyMargins = function() {
+            var marginLeft = $el.width() / 2,
+                marginTop = $el.height() / 2;
 
-			$el.css({
-				'margin-left': '-' + marginLeft + 'px',
-				'margin-top': '-' + marginTop + 'px',
-			});	
-		};
+            $el.css({
+                'margin-left': '-' + marginLeft + 'px',
+                'margin-top': '-' + marginTop + 'px',
+            });
+        };
 
-		// Calculate the scale factor
-		var scaleFactor = function() {
-			var scalingWidth = $wrapper.outerWidth() / $el.outerWidth(),
-				scalingHeight = $wrapper.outerHeight() / $el.outerHeight();
+        // Calculate the scale factor
+        var scaleFactor = function() {
+            var scalingWidth = $wrapper.outerWidth() / $el.outerWidth(),
+                scalingHeight = $wrapper.outerHeight() / $el.outerHeight();
 
-			return Math.min(scalingWidth, scalingHeight);
-		};
+            return Math.min(scalingWidth, scalingHeight);
+        };
 
-		// Apply the new scale factor
-		var applyScale = function() {
-			$el.css({
-				"transform": "scale(" + scaleFactor() + ")"
-			});
-		};
+        // Apply the new scale factor
+        var applyScale = function() {
+            $el.css({
+                "transform": "scale(" + scaleFactor() + ")"
+            });
+        };
 
-		// Capture resize
-		$(window).resize(throttle(applyScale, 100));
+        // Capture resize
+        $(window).resize(throttle(applyScale, 100));
 
-		// Goo!
-		applyMargins();
-		applyScale();
-	};
+        // Goo!
+        applyMargins();
+        applyScale();
+    };
 
 }(jQuery));
